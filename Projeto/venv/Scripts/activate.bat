326edf95627dd5830524061acb9cba88fa9a8959
João Neto <121770060+JaoNeto26@users.noreply.github.com>
2024-06-22 18:19:20 -0300

    update

--- Projeto/venv/Scripts/activate.bat
+++ Projeto/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\loren\Desktop\Projeto\venv
+set VIRTUAL_ENV=C:\Users\loren\Documents\GitHub\Site_melhorias\Projeto\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
